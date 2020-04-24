6f9a67b5f9def1fac75062868b2e9d647080452b
qamar929 <52096570+qamar929@users.noreply.github.com>
2020-04-25 00:30:05 +0500

    part 2 with course included

--- src/main/java/syedqamar/example/firstapp/controller/homeController.java
+++ src/main/java/syedqamar/example/firstapp/controller/homeController.java
@@ -8,7 +8,7 @@ public class homeController {
     @GetMapping("/")
     public String sayHelloGet()
     {
-        return "Hello World from get updated";
+        return "Welcome to Syed Qamar Abbas \n  \n      Urls for Course  \n POST: http://localhost:8080/course \n GET : http://localhost:8080/course \n GET | DELETE |UPDATE BY ID : http://localhost:8080/course \n \n    Urls for person  \n POST: http://localhost:8080/person \n GET : http://localhost:8080/person \n GET | DELETE |UPDATE BY ID : http://localhost:8080/person";
     }
 
 
